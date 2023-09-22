3bb0d612296b84a6bc9c2ea571e1099f57c77a44
srallingsELAM <srallings2023@elam.co.uk>
2023-09-22 11:38:13 +0100

    update

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,31 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class NewBehaviourScripts : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // Create a reference to the Rigidbody2D so we can manipulate it
+    Rigidbody2D playerObject;
+
+    //Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D playerObject;
+
+        //Find the Rigidbody2D component that is attached to the same object as this script
+        playerObject = gameObject.AddComponent<Rigidbody2D>();
     }
 
-
-    playerObject = GetComponent<Rigidbody2D>();
     // Update is called once per frame
     void Update()
     {
+
+        //create a 'float' that will be equal to the players horizontal input
         float movementValueX = Input.GetAxis("Horizontal");
+
+        //Change the X velocity of the rigidbody2D to be equal to the movement value
+        playerObject.velocity = new Vector2(movementValueX, playerObject.velocity.y);
+
     }
-
-    playerObject.velocity = new Vector2 (movementValueX, playerObject.velocity.y);
 }
-
-
-
-
-
-
-
-
-
-
