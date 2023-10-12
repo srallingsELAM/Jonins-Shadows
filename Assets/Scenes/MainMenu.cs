abfd532dae1f7f3a8328dd15b70e5fb360712aa5
srallingsELAM <srallings2023@elam.co.uk>
2023-10-12 12:45:34 +0100

    push

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
-
-
+public class MainMenu : MonoBehaviour
+{ 
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame ()
     {
-        Debug.Log("QUIT!");
         Application.Quit();
     }
 }
